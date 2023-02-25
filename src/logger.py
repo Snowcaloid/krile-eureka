ecfff54dc94c43644372d5763a573d1c53ef1303
Christopher Sharman <christopher.p.sharman@gmail.com>
2023-02-25 14:51:12 +0000

    #22 Enable logging to file
     - Add new output_to_file function in src/logger.py
     - Add LOG_FILE_FORMAT to .env
     - Update .env.example to include the LOG_FILE_FORMAT

--- src/logger.py
+++ src/logger.py
@@ -1,4 +1,6 @@
+import os
 import time
+from datetime import datetime
 
 import bot
 
@@ -29,3 +31,41 @@ async def guild_log_message(guild_id: int, message: str):
 
     # Print the log message with a timestamp
     await channel.send(f'<t:{int(time.time())}:t> {message}')
+
+    # Write this message to a log file
+    await output_to_file(guild_id, message)
+
+
+async def output_to_file(guild_id: int, message: str):
+    """Write a log message to a file.
+
+    Args:
+        guild_id (int): the id of the guild to write the log message to
+        message (str): the message to log
+
+    Returns:
+        None
+    """
+    # Get the log file format and replace the guild id placeholder
+    log_file = os.getenv('LOG_FILE_FORMAT')
+
+    if log_file is None or log_file == '':
+        return
+
+    # Set the guild id placeholder (do we want a guild_name placeholder too?)
+    log_file = log_file.replace('{guild_id}', str(guild_id))
+
+    datetime_now = datetime.now()
+
+    # Replace the time format placeholders with the current time
+    log_file = datetime_now.strftime(log_file)
+
+    # Get a date time prefix for the log message
+    log_time = datetime_now.strftime('%Y-%m-%d %H:%M:%S')
+
+    # Ensure the directory exists before writing to the file
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+
+    # Write the message to the file
+    with open(log_file, 'a') as file:
+        file.write(f'[{log_time}] {message}{os.linesep}')
