607fd3aa5819cefa7331ecff5aad34d2e595e710
Val <30471676+Snowcaloid@users.noreply.github.com>
2024-10-30 13:25:06 +0000

    added discord roles endpoint

--- src/api/register_handlers.py
+++ src/api/register_handlers.py
@@ -1,6 +1,7 @@
 from api.api_webserver import ApiRequestRegister
 from api.channels import ChannelsRequest
 from api.discord.channels import DiscordChannelsRequest
+from api.discord.roles import DiscordRolesRequest
 from api.guilds import GuildsRequest
 from api.login import LoginRequest
 from api.pings import PingsRequest
@@ -13,3 +14,4 @@ def register_api_handlers() -> None:
     ApiRequestRegister.register(ChannelsRequest)
     ApiRequestRegister.register(PingsRequest)
     ApiRequestRegister.register(DiscordChannelsRequest)
+    ApiRequestRegister.register(DiscordRolesRequest)
