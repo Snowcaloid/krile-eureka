6ee64e658728293da98d409a028d564829258125
Val <30471676+Snowcaloid@users.noreply.github.com>
2024-10-30 12:57:14 +0000

    extracted pings endpoint to own file

--- src/api/api_webserver.py
+++ src/api/api_webserver.py
@@ -8,8 +8,6 @@ from waitress import serve # type: ignore
 from nullsafe import _
 from uuid import UUID
 from data.db.sql import SQL, Record
-import bot
-from data.guilds.guild_pings import GuildPingType
 
 
 class WebserverUserCache:
@@ -85,73 +83,6 @@ class ApiRequestRegister:
                                    f'{request_type.url()}_post'),
                                methods=['GET']) # TODO: replace post when testing is done
         cls.HANDLERS.append(request_type)
-
-@webserver.get('/api/pings')
-def pings_get():
-    user_uuid = request.args.get('user')
-    if user_uuid is None: return [], 401
-    user_cache = _(webserver.users_cache)[UUID(user_uuid)]
-    if user_cache is None: return [], 401
-    result = []
-    for cached_guild in user_cache.guilds:
-        data = {'guild': cached_guild['id'], 'pings': []}
-        result.append(data)
-        pings_data = bot.instance.data.guilds.get(cached_guild['id']).pings
-        guild = bot.instance.get_guild(cached_guild['id'])
-        for ping_data in pings_data.all:
-            role = guild.get_role(ping_data.tag)
-            if role is None:
-                user = guild.get_member(ping_data.tag)
-            tag_type = 'role' if role is not None else 'user'
-            tag_name = role.name if role is not None else _(user).display_name
-            data['pings'].append({
-                'id': ping_data.id,
-                'tag_type': tag_type,
-                'tag': ping_data.tag,
-                'tag_name': tag_name,
-                'type': ping_data.type.name,
-                'event_type': ping_data.event_type})
-    return result
-
-@webserver.get('/api/post/pings')
-def pings_post():
-    user_uuid = request.args.get('user')
-    if user_uuid is None: return [], 401
-    user_cache = _(webserver.users_cache)[UUID(user_uuid)]
-    if user_cache is None: return [], 401
-    json = request.json
-    for json_guild in _(json):
-        guild_id = _(json_guild)['guild']
-        if guild_id not in (guild["id"] for guild in user_cache.guilds):
-            return f'Guild {_(json_guild)["guild"]} is not found in user cache for {user_cache.name}', 400
-        guild_data = bot.instance.data.guilds.get(guild_id)
-        guild = bot.instance.get_guild(guild_id)
-        if guild is None: return f'Guild {guild_id} not found', 400
-        for json_ping in _(json_guild)['pings']:
-            ping_id = _(json_ping)['id']
-            tag_type = _(json_ping)['tag_type']
-            tag = _(json_ping)['tag']
-            ping_type = GuildPingType[_(json_ping)['type']]
-            event_type = _(json_ping)['event_type']
-            if tag_type == 'role':
-                role = guild.get_role(tag)
-                if role is None: return f'Role {tag} not found', 400
-            elif tag_type == 'user':
-                user = guild.get_member(tag)
-                if user is None: return f'User {tag} not found', 400
-            else:
-                return f'Invalid tag type {tag_type}', 400
-            if not ping_id:
-                guild_data.pings.add_ping(ping_type, event_type, tag)
-            else:
-                ping_data = guild_data.pings.get_by_id(id)
-                if ping_data is None:
-                    return f'Ping {ping_id} not found', 400
-                elif _(json_ping)['delete']:
-                    guild_data.pings.remove_by_id(ping_id)
-                else:
-                    guild_data.pings.set_by_id(ping_id, function, event_type, tag)
-    return pings_get()
 
 def run():
     webserver.load_users_cache()
--- src/api/register_handlers.py
+++ src/api/register_handlers.py
@@ -2,6 +2,7 @@ from api.api_webserver import ApiRequestRegister
 from api.channels import ChannelsRequest
 from api.guilds import GuildsRequest
 from api.login import LoginRequest
+from api.pings import PingsRequest
 from api.roles import RolesRequest
 
 def register_api_handlers() -> None:
@@ -9,3 +10,4 @@ def register_api_handlers() -> None:
     ApiRequestRegister.register(RolesRequest)
     ApiRequestRegister.register(GuildsRequest)
     ApiRequestRegister.register(ChannelsRequest)
+    ApiRequestRegister.register(PingsRequest)
