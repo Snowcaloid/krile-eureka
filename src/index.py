e1c0e79eaca27bea180469654f2680db037e1f7a
Valentin Baron <valentin.baron@outlook.com>
2025-02-21 16:43:59 +0100

    removed initlogger for now

--- src/index.py
+++ src/index.py
@@ -21,29 +21,14 @@ async def on_ready():
     print(f'{bot.instance.user} has connected to Discord!')
     for guild in bot.instance.data.guilds.all:
         from logger import guild_log_message
-        from init_logger import InitLogger
         while not bot.instance.data.ready:
             print('Waiting for data to be ready...')
             await asyncio.sleep(500)
 
         message = (
             f'{bot.instance.user.mention} has successfully started.\n'
-            'Launch messages:\n'
         )
         await guild_log_message(guild.id, message)
-        launch_messages = InitLogger.content.splitlines(True)
-        limit = 2000 - 20 # leeway for the code block
-        message = ''
-        for line in launch_messages:
-            limit -= len(line)
-            if limit <= 0:
-                await guild_log_message(guild.id, f'```\n{message}```\n')
-                message = line
-                limit = 2000 - 20 - len(line)
-            else:
-                message += line
-        if message:
-            await guild_log_message(guild.id, f'```\n{message}```\n')
 
 
 bot.instance.run(os.getenv('DISCORD_TOKEN'))
--- src/init_logger.py
+++
@@ -1,7 +0,0 @@
-class InitLogger:
-    content: str = ''
-
-    @classmethod
-    def log(cl, message: str) -> None:
-        print(message)
-        InitLogger.content += f'{message}\n'
